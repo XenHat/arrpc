dec22a715ab04e2574b9d5e2bace83e79b644866
XenHat <me@xenh.at>
2025-03-11 23:09:39 -0400

    Add substantial performance improvements

--- src/process/index.js
+++ src/process/index.js
@@ -54,11 +54,23 @@ export default class ProcessServer {
         toCompare.push(p.replace('.exe', ''));
       }
 
+      // remove duplicates to process
+      const UniqueToCompare = []
+      for (const u of toCompare) {
+        if (!UniqueToCompare.includes(u)) {
+          UniqueToCompare.push(u)
+        }
+      }
+      
+      if (UniqueToCompare.length < 1) { // Skip empty path
+        continue
+      }
+
       for (const { executables, id, name } of DetectableDB) {
         if (executables?.some(x => {
           if (x.is_launcher) return false;
           // I can't figure how to not make 'sbin/init' NOT match 'Divinity Original Sin' with a substring match, so this ugly hack have to do for now
-          for (const comp of toCompare) {
+          for (const comp of UniqueToCompare) {
             const asdf = comp.includes('.exe')
             if (asdf) {
               // TODO: Do the reverse; check if x.name contains game/ or /games and THEN check against game/comp and games/comp
@@ -71,7 +83,7 @@ export default class ProcessServer {
               }
             }
           }
-          if (x.name[0] === '>' ? x.name.substring(1) !== toCompare[0] : !toCompare.some(y => x.name === y || `${cwdPath}/${y}`.includes(`/${x.name}`))) return false;
+          if (x.name[0] === '>' ? x.name.substring(1) !== UniqueToCompare[0] : !UniqueToCompare.some(y => x.name === y || `${cwdPath}/${y}`.includes(`/${x.name}`))) return false;
           if (args && x.arguments) return args.join(" ").indexOf(x.arguments) > -1;
           return true;
         })) {
